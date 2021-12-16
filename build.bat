b32727301845d448a989c65ab18e07dda4cab660
Nikita Fediuchin <cfnptr@gmail.com>
2021-12-16 10:49:07 +0200

    Improve build scripts

--- build.bat
+++ build.bat
@@ -1,14 +1,22 @@
-ECHO OFF
+@ECHO OFF
+
+cmake --version
 
 IF NOT %ERRORLEVEL% == 0 (
     ECHO Failed to get CMake version, please check if it's installed.
 )
+
+ECHO(
+ECHO Configuring project...
 
 cmake -DCMAKE_BUILD_TYPE=Release -S . -B build/
 
 IF NOT %ERRORLEVEL% == 0 (
     ECHO Failed to configure CMake project.
 )
+
+ECHO(
+ECHO Building project...
 
 cmake --build build/
 
--- build.sh
+++ build.sh
@@ -6,9 +6,15 @@ if ! cmake --version ; then
     echo "Failed to get CMake version, please check if it's installed."
 fi
 
+echo ""
+echo "Configuring project..."
+
 if ! cmake -DCMAKE_BUILD_TYPE=Release -S . -B build/ ; then
     echo "Failed to configure CMake project."
 fi
+
+echo ""
+echo "Building project..."
 
 if ! cmake --build build/ ; then
     echo "Failed to build CMake project."
